8a70e9efb93846fedcdccc721fdfc8fb5d0ef774
Incognito009 <76397211+Incognito009@users.noreply.github.com>
2021-07-10 11:44:46 +0530

    Update translate.py

--- deegram/utils/translate.py
+++ deegram/utils/translate.py
@@ -1,7 +1,7 @@
 VERSION = "0.2.4"
 CREATOR = "akhil"
 BOT_NAME = "Musically"
-DONATE = "https://paypal.me/hafitzsetya1"
+DONATE = "only smile"
 WELCOME_MSG = (
     f"🎶🎵 Welcome to {BOT_NAME} 🎵🎶\n\n"
     "Click /help for more information."
